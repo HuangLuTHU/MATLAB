a91913b038e0c78e83045bad2b4eae201d99a6e9
Huang Lu <huanglu.thu@outlook.com>
2015-11-01 13:37:20 +0800

    Update hw3_2.m

--- hw3_2.m
+++ hw3_2.m
@@ -1,5 +1,5 @@
 % Created by Huang Lu, Department of Electronic Engineering, Tsinghua University
-% 2015/10/31, Friday, Beijing
+% 2015/10/31, Saturday, Beijing
 % Code for Homework 3 of Communication and Network
 
 % Generate the Original Signal
